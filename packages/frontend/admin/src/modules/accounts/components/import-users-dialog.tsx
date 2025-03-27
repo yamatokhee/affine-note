57ebb6baba74bfbe18de19497f686c89447c3e2c
JimmFly <yangjinfei001@gmail.com>
2025-03-27 07:10:15 +0000

    chore: adjust CSV template button style (#11226)
    close AF-2396

--- packages/frontend/admin/src/modules/accounts/components/import-users-dialog.tsx
+++ packages/frontend/admin/src/modules/accounts/components/import-users-dialog.tsx
@@ -248,7 +248,7 @@ export function ImportUsersDialog({
             <>
               <div
                 onClick={downloadCsvTemplate}
-                className="mb-2 sm:mb-0 text-[15px] px-4 py-2 h-10 underline cursor-pointer"
+                className="mb-2 sm:mb-0 text-[15px] px-0 py-2 h-10 underline cursor-pointer"
               >
                 CSV template
               </div>
