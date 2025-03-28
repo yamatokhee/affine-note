a459a00b218249c27b0cb651923d41dd5f3a6973
donteatfriedrice <chs@bupt.cn>
2025-03-28 02:25:00 +0000

    feat(editor): support convert edgeless bookmark to embed iframe block (#11237)
    Close [BS-2941](https://linear.app/affine-design/issue/BS-2941/白板-surface-中-card-view-无法转为-embed-view)

--- blocksuite/affine/blocks/block-embed/src/embed-iframe-block/index.ts
+++ blocksuite/affine/blocks/block-embed/src/embed-iframe-block/index.ts
@@ -1,5 +1,10 @@
 export * from './adapters';
 export * from './commands';
 export * from './configs';
+export {
+  EMBED_IFRAME_DEFAULT_HEIGHT_IN_SURFACE,
+  EMBED_IFRAME_DEFAULT_WIDTH_IN_SURFACE,
+} from './consts';
 export * from './embed-iframe-block';
 export * from './embed-iframe-spec';
+export { canEmbedAsIframe } from './utils';
