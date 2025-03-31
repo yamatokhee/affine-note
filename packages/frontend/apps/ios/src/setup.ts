5f3cf538198154e2239e1dfcf5efe8c747c7fcca
EYHN <cneyhn@gmail.com>
2025-03-31 14:20:41 +0000

    feat(core): cleanup unused indexeddb when bootstrapping (#11325)

--- packages/frontend/apps/ios/src/setup.ts
+++ packages/frontend/apps/ios/src/setup.ts
@@ -1,3 +1,4 @@
 import '@affine/core/bootstrap/browser';
 import '@affine/core/bootstrap/blocksuite';
+import '@affine/core/bootstrap/cleanup';
 import './proxy';
--- packages/frontend/apps/mobile/src/setup.ts
+++ packages/frontend/apps/mobile/src/setup.ts
@@ -1,4 +1,5 @@
 import '@affine/core/bootstrap/browser';
 import '@affine/core/bootstrap/blocksuite';
+import '@affine/core/bootstrap/cleanup';
 import '@affine/component/theme';
 import '@affine/core/mobile/styles/mobile.css';
